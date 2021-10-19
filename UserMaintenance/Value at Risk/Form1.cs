44ea7797a25ebd8443210b2e72a7128d5ddcc7f1
Ambrus Tóth <ambrus.toth@stud.uni-corvinus.hu>
2021-10-19 23:24:33 +0200

    4 feladat

--- UserMaintenance/Value at Risk/Form1.cs
+++ UserMaintenance/Value at Risk/Form1.cs
@@ -12,9 +12,13 @@ namespace Value_at_Risk
 {
     public partial class Form1 : Form
     {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
         }
     }
 }

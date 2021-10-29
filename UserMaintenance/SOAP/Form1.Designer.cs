ae384e85ace81224b7ab8460ffc11327f56687fa
Ambrus Tóth <ambrus.toth@stud.uni-corvinus.hu>
2021-10-29 22:10:07 +0200

    7. feladat

--- UserMaintenance/SOAP/Form1.Designer.cs
+++ UserMaintenance/SOAP/Form1.Designer.cs
@@ -29,11 +29,14 @@ namespace SOAP
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.chartRateData = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.TolPicker = new System.Windows.Forms.DateTimePicker();
+            this.IgPicker = new System.Windows.Forms.DateTimePicker();
+            this.cbxValuta = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chartRateData)).BeginInit();
             this.SuspendLayout();
@@ -41,32 +44,66 @@ namespace SOAP
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 46);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(240, 150);
             this.dataGridView1.TabIndex = 0;
             // 
             // chartRateData
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chartRateData.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chartRateData.Legends.Add(legend1);
-            this.chartRateData.Location = new System.Drawing.Point(295, 13);
+            chartArea3.Name = "ChartArea1";
+            this.chartRateData.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chartRateData.Legends.Add(legend3);
+            this.chartRateData.Location = new System.Drawing.Point(286, 46);
             this.chartRateData.Name = "chartRateData";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chartRateData.Series.Add(series1);
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chartRateData.Series.Add(series3);
             this.chartRateData.Size = new System.Drawing.Size(300, 300);
             this.chartRateData.TabIndex = 1;
             this.chartRateData.Text = "chart1";
+            // 
+            // TolPicker
+            // 
+            this.TolPicker.Location = new System.Drawing.Point(13, 20);
+            this.TolPicker.Name = "TolPicker";
+            this.TolPicker.Size = new System.Drawing.Size(200, 20);
+            this.TolPicker.TabIndex = 2;
+            this.TolPicker.Value = new System.DateTime(2020, 1, 1, 21, 56, 0, 0);
+            this.TolPicker.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // IgPicker
+            // 
+            this.IgPicker.Location = new System.Drawing.Point(220, 19);
+            this.IgPicker.Name = "IgPicker";
+            this.IgPicker.Size = new System.Drawing.Size(200, 20);
+            this.IgPicker.TabIndex = 3;
+            this.IgPicker.Value = new System.DateTime(2020, 6, 30, 21, 56, 0, 0);
+            this.IgPicker.ValueChanged += new System.EventHandler(this.dateTimePicker2_ValueChanged);
+            // 
+            // cbxValuta
+            // 
+            this.cbxValuta.FormattingEnabled = true;
+            this.cbxValuta.Items.AddRange(new object[] {
+            "EUR",
+            "USD"});
+            this.cbxValuta.Location = new System.Drawing.Point(427, 18);
+            this.cbxValuta.Name = "cbxValuta";
+            this.cbxValuta.Size = new System.Drawing.Size(121, 21);
+            this.cbxValuta.TabIndex = 4;
+            this.cbxValuta.Text = "EUR";
+            this.cbxValuta.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cbxValuta);
+            this.Controls.Add(this.IgPicker);
+            this.Controls.Add(this.TolPicker);
             this.Controls.Add(this.chartRateData);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
@@ -81,6 +118,9 @@ namespace SOAP
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataVisualization.Charting.Chart chartRateData;
+        private System.Windows.Forms.DateTimePicker TolPicker;
+        private System.Windows.Forms.DateTimePicker IgPicker;
+        private System.Windows.Forms.ComboBox cbxValuta;
     }
 }
 
--- UserMaintenance/SOAP/Form1.cs
+++ UserMaintenance/SOAP/Form1.cs
@@ -20,11 +20,36 @@ namespace SOAP
         public Form1()
         {
             InitializeComponent();
-            GetExchangeRates();
-            dataGridView1.DataSource = Rates;
+            RefreshData();
         }
 
+        private void RefreshData()
+        {
+            Rates.Clear();
+            GetExchangeRates();
+            dataGridView1.DataSource = Rates;
+            Charting();
+        }
 
+        private void Charting()
+        {
+            chartRateData.DataSource = Rates;
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
+            
+        }
 
         private void GetExchangeRates()
         {
@@ -34,12 +59,11 @@ namespace SOAP
             // Ebben a formában azonban olvashatóbb a kód, és változtatás esetén elég egy helyen átírni az osztály típusát.
             var mnbService = new MNBArfolyamServiceSoapClient();
 
-            var request = new GetExchangeRatesRequestBody()
-            {
-                currencyNames = "EUR",
-                startDate = "2020-01-01",
-                endDate = "2020-06-30"
-            };
+            var request = new GetExchangeRatesRequestBody();
+            request.currencyNames = cbxValuta.SelectedItem.ToString();
+            request.startDate = TolPicker.Value.ToString("yyyy-MM-dd");
+            request.endDate = IgPicker.Value.ToString("yyyy-MM-dd");
+
 
             // Ebben az esetben a "var" a GetExchangeRates visszatérési értékéből kapja a típusát.
             // Ezért a response változó valójában GetExchangeRatesResponseBody típusú.
@@ -74,22 +98,23 @@ namespace SOAP
                 if (unit != 0)
                     rate.Value = value / unit;
 
-                chartRateData.DataSource = Rates;
-
-                var series = chartRateData.Series[0];
-                series.ChartType = SeriesChartType.Line;
-                series.XValueMember = "Date";
-                series.YValueMembers = "Value";
-                series.BorderWidth = 2;
-
-                var legend = chartRateData.Legends[0];
-                legend.Enabled = false;
-
-                var chartArea = chartRateData.ChartAreas[0];
-                chartArea.AxisX.MajorGrid.Enabled = false;
-                chartArea.AxisY.MajorGrid.Enabled = false;
-                chartArea.AxisY.IsStartedFromZero = false;
+                
             }
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshData();
         }
     }
 }
